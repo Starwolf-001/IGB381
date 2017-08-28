98c499400cc845f174e526f329220bb8615ae7f9
Starwolf-001 <the.Starwolf.001@gmail.com>
2017-08-28 18:38:55 +1000

    Added current speed of sun orbit to UI.

--- Assignment 1/Assignment_1/Assets/SunAndMoon.cs
+++ Assignment 1/Assignment_1/Assets/SunAndMoon.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SunAndMoon : MonoBehaviour {
 
     public int speed;
+    public Text textSpeed;
 
     // Use this for initialization
     void Start ()
@@ -16,7 +18,18 @@ public class SunAndMoon : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        Orbit();
+        CurrentSpeedUI();
+    }
+
+    void Orbit()
+    {
         transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
         transform.LookAt(Vector3.zero);
+    }
+
+    void CurrentSpeedUI()
+    {
+        textSpeed.text = speed.ToString();
     }
 }
