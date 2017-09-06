0ccafbb50e5d40ed8dcc7eae18de95949b0a5a08
Starwolf-001 <the.Starwolf.001@gmail.com>
2017-09-06 12:11:49 +1000

    Pivoting spotlight scripts added

---
+++ Assignment 1/Assignment_1/Assets/Lightning/Scripts/NorthLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NorthLight : MonoBehaviour {
+
+    public int speed;
+    public float angle;
+    public Text textSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Default Speed
+        speed = 1;
+        // Default angle
+        angle = 25f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speed != 0)
+        {
+            Pivot();
+        }
+        CurrentSpeedUI();
+    }
+
+    void Pivot()
+    {
+        transform.rotation = Quaternion.Euler(0f, 90f + angle * Mathf.Sin(Time.time * speed), 0f);
+    }
+
+    void CurrentSpeedUI()
+    {
+        textSpeed.text = speed.ToString();
+    }
+}
